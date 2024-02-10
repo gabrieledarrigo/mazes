e59a77f02813755e5f0f88cd8d1d7b67b6ac80ff
Gabriele D'Arrigo <darrigo.g@gmail.com>
2024-02-10 09:22:22 +0100

    Adapt the Grid struct to be iterable

--- src/grid.rs
+++ src/grid.rs
@@ -1,3 +1,5 @@
+use std::{iter::Flatten, slice::Iter};
+
 use rand::Rng;
 
 use crate::cell::Cell;
@@ -6,7 +8,7 @@ use crate::cell::Cell;
 pub struct Grid {
     rows: i32,
     columns: i32,
-    grid: Vec<Vec<Cell>>,
+    cells: Vec<Vec<Cell>>,
 }
 
 impl Grid {
@@ -21,12 +23,12 @@ impl Grid {
     ///
     /// A new `Grid` instance.
     pub fn new(rows: i32, columns: i32) -> Self {
-        let grid = Self::prepare_grid(rows, columns);
+        let cells = Self::prepare_grid(rows, columns);
 
         Self {
             rows,
             columns,
-            grid,
+            cells,
         }
     }
 
@@ -41,7 +43,7 @@ impl Grid {
     ///
     /// A vector of vectors representing the grid.
     fn prepare_grid(rows: i32, columns: i32) -> Vec<Vec<Cell>> {
-        let mut grid = vec![vec![Cell::new(0, 0); columns as usize]; rows as usize];
+        let mut cells = vec![vec![Cell::new(0, 0); columns as usize]; rows as usize];
 
         for row in 0..rows {
             for column in 0..columns {
@@ -63,11 +65,11 @@ impl Grid {
                     cell.set_east(Some((row, column + 1)));
                 }
 
-                grid[row as usize][column as usize] = cell;
+                cells[row as usize][column as usize] = cell;
             }
         }
 
-        grid
+        cells
     }
 
     /// Return the cell at the specified row and column.
@@ -80,13 +82,13 @@ impl Grid {
     /// # Returns
     ///
     /// An optional reference to the cell at the specified position.
-    fn cell(&self, row: i32, column: i32) -> Option<&Cell> {
+    pub fn cell(&self, row: i32, column: i32) -> Option<&Cell> {
         if (row >= 0 && row < self.rows) == false || (column >= 0 && column < self.columns) == false
         {
             return None;
         }
 
-        Some(&self.grid[row as usize][column as usize])
+        Some(&self.cells[row as usize][column as usize])
     }
 
     /// Return a random cell from the grid.
@@ -94,12 +96,54 @@ impl Grid {
     /// # Returns
     ///
     /// An optional reference to a random cell from the grid.
-    fn random_cell(&self) -> Option<&Cell> {
+    pub fn random_cell(&self) -> Option<&Cell> {
         let mut rng = rand::thread_rng();
         let row = rng.gen_range(0..self.rows);
         let column = rng.gen_range(0..self.columns);
 
         self.cell(row, column)
+    }
+
+    /// Return a struct to iterate the cells.
+    ///
+    /// # Returns
+    ///
+    /// A GridIterator struct
+    pub fn iter(&self) -> GridIterator {
+        GridIterator::new(self.cells.iter().flatten())
+    }
+}
+
+/// Represents an iterator over the cells in a grid.
+pub struct GridIterator<'a> {
+    cells: Flatten<Iter<'a, Vec<Cell>>>,
+}
+
+impl<'a> GridIterator<'a> {
+    /// Creates a new `GridIterator` instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `cells` - The iterator over the cells in the grid.
+    ///
+    /// # Returns
+    ///
+    /// A new `GridIterator` instance.
+    pub fn new(cells: Flatten<Iter<'a, Vec<Cell>>>) -> Self {
+        Self { cells }
+    }
+}
+
+impl<'a> Iterator for GridIterator<'a> {
+    type Item = &'a Cell;
+
+    /// Advances the iterator and returns the next cell in the grid.
+    ///
+    /// # Returns
+    ///
+    /// An optional reference to the next cell in the grid.
+    fn next(&mut self) -> Option<Self::Item> {
+        self.cells.next()
     }
 }
 
@@ -118,7 +162,7 @@ mod tests {
 
         for row in 0..rows {
             for column in 0..columns {
-                let cell = &grid.grid[row as usize][column as usize];
+                let cell = &grid.cells[row as usize][column as usize];
 
                 if row > 0 {
                     assert_eq!(cell.north(), Some((row - 1, column)));
@@ -168,5 +212,26 @@ mod tests {
         let invalid_cell = grid.cell(invalid_row, invalid_column);
 
         assert!(invalid_cell.is_none());
+    }
+
+    #[test]
+    fn test_iter() {
+        let rows = 3;
+        let columns = 3;
+        let grid = Grid::new(rows, columns);
+
+        let mut iter = grid.iter();
+
+        for row in 0..rows {
+            for column in 0..columns {
+                let cell = iter.next();
+
+                assert!(cell.is_some());
+                assert_eq!(cell.unwrap().row(), row);
+                assert_eq!(cell.unwrap().column(), column);
+            }
+        }
+
+        assert!(iter.next().is_none());
     }
 }
