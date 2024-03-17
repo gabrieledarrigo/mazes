0308e0d447a77787ddc05e6e7fc203f4206655ff
Gabriele D'Arrigo <darrigo.g@gmail.com>
2024-03-17 13:59:30 +0100

    Better imports on main

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,36 @@
 mod algorithms;
 mod grids;
 
-use algorithms::{binary_tree::*, sidewinder::*};
-use grids::distance_grid::DistanceGrid;
-use grids::grid::Grid;
-
-use crate::algorithms::aldous_broder::AldousBroder;
-use crate::grids::base_grid::BaseGrid;
+use algorithms::{aldous_broder::AldousBroder, binary_tree::BinaryTree, sidewinder::Sidewinder};
+use grids::{base_grid::BaseGrid, distance_grid::DistanceGrid, grid::Grid};
 
 fn main() {
     println!("Binary tree");
-    let mut grid = Grid::new(5, 5);
+    let mut grid = Grid::new(6, 6);
     BinaryTree::on(&mut grid);
     println!("{}", grid.display());
 
     println!("Sidewinder");
-    let mut grid_1 = Grid::new(5, 5);
+    let mut grid_1 = Grid::new(6, 6);
     Sidewinder::on(&mut grid_1);
     println!("{}", grid_1.display());
 
     println!("With distances");
-    let mut distance_grid = DistanceGrid::new(5, 5);
+    let mut distance_grid = DistanceGrid::new(6, 6);
     BinaryTree::on(&mut distance_grid);
     println!("{}", distance_grid.display());
 
     println!("With path from north west to south east");
     println!(
         "{}",
-        distance_grid.display_path_to(distance_grid.cell(4, 0).unwrap().clone())
+        distance_grid.display_path_to(distance_grid.cell(5, 0).unwrap().clone())
     );
 
     println!("With colors");
     println!("{}", distance_grid.display_with_color());
 
     println!("Aldous Brother");
-    let mut grid_2 = Grid::new(5, 5);
+    let mut grid_2 = DistanceGrid::new(6, 6);
     AldousBroder::on(&mut grid_2);
-    println!("{}", grid_2.display());
+    println!("{}", grid_2.display_with_color());
 }
