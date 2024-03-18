94cf83cc7737303ee3c4a3e8b71e1852b579be3e
Gabriele D'Arrigo <darrigo.g@gmail.com>
2024-03-18 21:23:37 +0100

    Add more documentation

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,6 +1,15 @@
 use crate::grids::base_grid::{BaseGrid, GridCell};
 use rand::Rng;
 
+/// Returns a random cell from the given grid.
+///
+/// # Arguments
+///
+/// * `grid` - The grid from which to select a random cell.
+///
+/// # Returns
+///
+/// The randomly selected `GridCell`.
 pub fn random_cell(grid: &impl BaseGrid) -> GridCell {
     let mut rng = rand::thread_rng();
     let row = rng.gen_range(0..grid.rows());
@@ -9,6 +18,16 @@ pub fn random_cell(grid: &impl BaseGrid) -> GridCell {
     grid.cell(row, column).unwrap().clone()
 }
 
+/// Returns a random neighbor of the given cell in the given grid.
+///
+/// # Arguments
+///
+/// * `grid` - The grid in which the cell is located.
+/// * `cell` - The cell for which to find a random neighbor.
+///
+/// # Returns
+///
+/// The randomly selected neighbor `GridCell`.
 pub fn random_neighbor(grid: &impl BaseGrid, cell: GridCell) -> GridCell {
     let mut rng = rand::thread_rng();
 
